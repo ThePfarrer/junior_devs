f5709bc7c7c552bb26342e4ae25cba8e7aa2396e
Uche Okereke <orezpablo@gmail.com>
2024-10-25 03:23:01 +0200

    Step 3&4: Concatenate files and number std output

--- Python/cli_cat_tool/src/cat_tool/cli.py
+++ Python/cli_cat_tool/src/cat_tool/cli.py
@@ -7,20 +7,26 @@ from .cat import cat, process
 
 def read_stdin():
     """Read content from stdin"""
-    return sys.stdin.read()
+    return sys.stdin.read().rstrip()
 
 
 @click.command()
-@click.argument('file_path')
-def main(file_path):
+@click.option('-n', is_flag=True)
+@click.argument('file_path', nargs=-1)
+def main(n, file_path):
     """Read from a file or stdin if file_path is '-'"""
+    if n:
+        file_contents = read_stdin().split("\n")
+        for indx, content in enumerate(file_contents, 1):
+            click.echo(f"{indx} {content}")
     if file_path == '-':
         file_contents = read_stdin()
-        click.echo(file_contents, nl=False)
+        click.echo(file_contents)
     else:
-        file_contents = cat(file_path)
-        for chunk in file_contents:
-            click.echo(process(chunk))
+        for files in file_path:
+            file_contents = cat(files)
+            for chunk in file_contents:
+                click.echo(process(chunk), nl=False)
 
 
 if __name__ == '__main__':
