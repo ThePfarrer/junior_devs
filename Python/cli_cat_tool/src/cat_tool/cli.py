e00af6783609a38556b6598e1d1c70546eacaaf3
Uche Okereke <orezpablo@gmail.com>
2024-10-25 02:30:28 +0200

    Step 2: Read input from standard in

--- Python/cli_cat_tool/src/cat_tool/cli.py
+++ Python/cli_cat_tool/src/cat_tool/cli.py
@@ -1,14 +1,26 @@
+import sys
+
 import click
 
-from .cat import cat
+from .cat import cat, process
+
+
+def read_stdin():
+    """Read content from stdin"""
+    return sys.stdin.read()
 
 
 @click.command()
 @click.argument('file_path')
 def main(file_path):
-    """Read the file to the CLI"""
-    file_contents = cat(file_path)
-    print(file_contents)
+    """Read from a file or stdin if file_path is '-'"""
+    if file_path == '-':
+        file_contents = read_stdin()
+        click.echo(file_contents, nl=False)
+    else:
+        file_contents = cat(file_path)
+        for chunk in file_contents:
+            click.echo(process(chunk))
 
 
 if __name__ == '__main__':
