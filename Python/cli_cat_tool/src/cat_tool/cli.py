c26cdddb6b2aff4a92d8a0d41bcbcda07d8b3642
Uche Okereke <orezpablo@gmail.com>
2024-10-30 14:40:21 +0100

    Step 5: Number both blank and non-blank lines

--- Python/cli_cat_tool/src/cat_tool/cli.py
+++ Python/cli_cat_tool/src/cat_tool/cli.py
@@ -1,32 +1,45 @@
-import sys
-
 import click
 
-from .cat import cat, process
-
-
-def read_stdin():
-    """Read content from stdin"""
-    return sys.stdin.read().rstrip()
+from .cat import cat
 
 
 @click.command()
-@click.option('-n', is_flag=True)
+@click.option('-n', 'number', is_flag=True, help='number all output lines')
+@click.option('-b', 'number_nonblank', is_flag=True, help='number nonempty output lines, overrides -n')
 @click.argument('file_path', nargs=-1)
-def main(n, file_path):
-    """Read from a file or stdin if file_path is '-'"""
-    if n:
-        file_contents = read_stdin().split("\n")
-        for indx, content in enumerate(file_contents, 1):
-            click.echo(f"{indx} {content}")
-    if file_path == '-':
-        file_contents = read_stdin()
-        click.echo(file_contents)
-    else:
-        for files in file_path:
-            file_contents = cat(files)
+def main(number, number_nonblank, file_path):
+    """
+    Concatenate FILE(s) to standard output.
+
+    With no FILE, or when FILE is -, read standard input.
+"""
+
+    if file_path:
+        if file_path[0] == '-' and len(file_path) == 1:
+            file_contents = cat()
             for chunk in file_contents:
-                click.echo(process(chunk), nl=False)
+                click.echo(chunk)
+        else:
+            for files in file_path:
+                file_contents = cat(files)
+                for chunk in file_contents:
+                    click.echo(chunk, nl=False)
+    else:
+        file_contents = cat()
+        for chunk in file_contents:
+            chunk = chunk.split("\n")
+            if number:
+                for indx, content in enumerate(chunk, 1):
+                    click.echo(f"     {indx}  {content}")
+
+            if number_nonblank:
+                indx = 1
+                for content in chunk:
+                    if content:
+                        click.echo(f"     {indx}  {content}")
+                        indx += 1
+                    else:
+                        click.echo()
 
 
 if __name__ == '__main__':
