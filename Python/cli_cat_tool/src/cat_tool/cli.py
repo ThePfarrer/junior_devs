2eaa47646f28998e1df3db2db5d837389f3a2a25
Uche Okereke <orezpablo@gmail.com>
2024-11-05 21:37:47 +0100

    Fix line number counter issue

--- Python/cli_cat_tool/src/cat_tool/cli.py
+++ Python/cli_cat_tool/src/cat_tool/cli.py
@@ -3,6 +3,14 @@ import click
 from .cat import cat
 
 
+class CounterClass:
+    def __init__(self):
+        self.counter = 1
+
+    def increment(self):
+        self.counter += 1
+
+
 @click.command()
 @click.option("-n", "number", is_flag=True, help="number all output lines")
 @click.option(
@@ -19,43 +27,45 @@ def main(number, number_nonblank, file_path):
     With no FILE, or when FILE is -, read standard input.
     """
 
+    line_number = CounterClass()
+
     if not file_path or (file_path == ("-",)):
         file_contents = cat()
         for chunk in file_contents:
-            display_with_line_numbers(chunk, number, number_nonblank)
+            display_with_line_numbers(chunk, line_number, number, number_nonblank)
     else:
         for files in file_path:
-            output_file_contents(files, number, number_nonblank)
+            output_file_contents(files, line_number, number, number_nonblank)
 
 
-def display_with_line_numbers(content, number=False, number_nonblank=False):
+def display_with_line_numbers(content, counter, number=False, number_nonblank=False):
     """
     Display content with line numbers if specified.
     """
     lines = content.split("\n")
-    line_number = 1
     for line in lines:
+        line_number_format = str(counter.counter).rjust(6, " ")
         if number:
-            click.echo(f"     {line_number}  {line}")
-            line_number += 1
+            click.echo(f"{line_number_format}  {line}")
+            counter.increment()
 
         elif number_nonblank:
             if line:
-                click.echo(f"     {line_number}  {line}")
-                line_number += 1
+                click.echo(f"{line_number_format}  {line}")
+                counter.increment()
             else:
                 click.echo()
         else:
             click.echo(line)
 
 
-def output_file_contents(file_path, number, number_nonblank):
+def output_file_contents(file_path, counter, number, number_nonblank):
     """
     Outputs the contents of a file, with optional line numbering.
     """
     file_contents = cat(file_path)
     for chunk in file_contents:
-        display_with_line_numbers(chunk, number, number_nonblank)
+        display_with_line_numbers(chunk, counter, number, number_nonblank)
 
 
 if __name__ == "__main__":
